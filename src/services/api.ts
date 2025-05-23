adee1e14efef85117ee2dcddb7290b263fa0d3e2
Kateryna Odnoroh <katya.odnorog@gmail.com>
2025-05-23 19:21:55 +0300

    fix2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { TypeImages } from "../types/types";
 
+interface Results {
+  total: number;
+  total_pages: number;
+  results: TypeImages[];
+}
+
 const API_KEY = "ZCD1CHwvydMIyCuXcUYkpyBDWUiD3Ud07IM_9OnUF4Y";
 const BASE_URL = "https://api.unsplash.com/search/photos";
 
 const findImages = async (
   query: string,
   page: number = 1
-): Promise<TypeImages> => {
-  const response = await axios.get<TypeImages>(BASE_URL, {
+): Promise<Results> => {
+  const response = await axios.get<Results>(BASE_URL, {
     headers: {
       Authorization: `Client-ID ${API_KEY}`,
     },
